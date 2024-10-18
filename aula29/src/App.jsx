90b625fa0d526cd6474f29a6e50b596ef3d4701c
Cleber França <cleber.francaa12@gmail.com>
2024-10-18 12:02:41 -0300

    metodos

---
+++ aula29/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Comp1 from './componentes/Comp1'
+
+function App() {
+  
+  let cor = 'purple'
+  let esp = '10px'
+
+
+  let nome = ''
+  let idade = ''
+  let profissao = ''
+
+  function carregarDados(){
+    nome = document.getElementById('nome').value
+    idade = document.getElementById('idade').value
+    profissao = document.getElementById('profissao').value
+  }
+  
+
+  return (
+          <>
+            <h1>Titulos</h1>
+
+            <input id='nome' type='text' placeholder='Digite seu nome'/><br></br>
+            <input id='idade' type='text' placeholder='Digite a idade'/><br></br>
+            <input id='profissao' type='text' placeholder='Digite a profissão'/><br></br>
+            <button onClick={carregarDados} type='button'>Enviar</button>
+
+            <Comp1 nome={nome} idade={idade} profissao={profissao} />
+          </>
+  )
+}
+
+export default App
